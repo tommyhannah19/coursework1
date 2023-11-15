d460de2fce4183926e14056c9cf02e73d34695e5
tommyhannah19 <thanna201@caledonian.ac.uk>
2023-11-15 13:44:18 +0000

    added an input check for no argument provided

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
-class Dex2Hex
-{
-
+class Dex2Hex {
     public static int Arg1;
 
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+    public static void main(String args[]) {
+        // Check if input argument is provided
+        if (args.length == 0) {
+            System.err.println("Error: No input argument provided. Please provide a decimal value.");
+            System.exit(1); // Exit with an error code
+        } else {
+            Arg1 = Integer.parseInt(args[0]);
+            
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int rem, num;
+            num = Arg1;
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
         }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
     }
 }
